beab4cb4a3074d28a4562dcf4c065cf795124d6c
atanuchina0709 <atanuchina07092002@gmail.com>
2025-11-15 16:23:20 +0530

    again changes

--- API/Program.cs
+++ API/Program.cs
@@ -67,8 +67,12 @@ builder.Services.AddAuthorizationBuilder()
 
 
 var app = builder.Build();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+    "https://agreeable-bay-0c7a33900.3.azurestaticapps.net",
+    "http://localhost:4200", 
+    "https://localhost:4200"));
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
